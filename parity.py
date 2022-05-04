debc4512b4079d082a2d0edd3f0eaa71d192d59b
MikePfunk28 <47832556+MikePfunk28@users.noreply.github.com>
2022-05-04 06:14:27 -0400

    Create parity.py

---
+++ parity.py
@@ -0,0 +1,16 @@
+def main():
+    x = int(input("what's x? ")) # function is_even() takes a number,
+    if is_even(x):               # n and uses mod to determine even or odd
+        print("Even")
+    else:
+        print("Odd")
+
+# is_even(n) takes user input x as it takes n, above we pass x into is_even(x)
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+main()
