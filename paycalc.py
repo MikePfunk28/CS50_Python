25fea741f27c163590cae22633380888ae767c3b
MikePfunk28 <47832556+MikePfunk28@users.noreply.github.com>
2022-06-03 16:59:48 -0400

    Update paycalc.py
    modified paycalc as I had same line of code.

--- paycalc.py
+++ paycalc.py
@@ -4,7 +4,7 @@ rate = float(input("Enter Rate:"))
 if hrs > 40:
     overrate = rate * 1.5
     overhrs = hrs - 40
-pay = hrs * rate
+# pay = hrs * rate - this would get overwritten by then next line and no errors
 pay = (hrs - overhrs) * rate
 overpay = overrate * overhrs
 gross = overpay + pay
