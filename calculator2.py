bd3f23fad984d1ac0a2406fb47ecf18c6e37fee0
MikePfunk28 <47832556+MikePfunk28@users.noreply.github.com>
2022-06-07 09:54:03 -0400

    Update calculator2.py
    added notes in comments, good things to remember.

--- calculator2.py
+++ calculator2.py
@@ -1,8 +1,20 @@
 def main():
-    x = int(input("What's x? "))
-    print("x squared is", square(x))
+    try:
+        x = int(input("What's x? "))
+    except ValueError:
+        print("x needs to be an integer")
+    # Without the else in the try and except, the print statement would
+    # only print if given an integer, but if given a string, it would
+    # error UnboundLocalError after still executing the except statement.
+    # Could probably break and exit if bad value, but this works as well 
+    # and I havent tested the other case.
+    else:    
+        print("x squared is", square(x))
+
 
 def square(n):
     return pow(n, 2) # n** 2 or n * n or return pow(n, 2)
 
-main()
+
+if __name__ == "__main__":
+    main()
